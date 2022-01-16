81f0d0b5e57db8313058d2880afcf2cfd1c618bb
hossainsafwan <safwanhossain@live.com>
2022-01-16 02:07:51 -0500

    Create Entity, DAO and Database

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,7 +16,30 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao{
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun query(night: SleepNight)
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightID = :key")
+    fun get(key: Long): SleepNight?
+
+    @Query("DELETE from daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * from daily_sleep_quality_table ORDER BY nightID DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
+    @Query("SELECT * from daily_sleep_quality_table ORDER BY nightID")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+}
